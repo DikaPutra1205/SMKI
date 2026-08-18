54c578fdab164a77efee20baa7ca57cc8970507c
Elmanda1 <falihelmanda@gmail.com>
2026-08-18 20:00:48 +0700

    feat(reports): implement executive pdf audit report generator

--- app/Http/Controllers/Web/ReportExportController.php
+++ app/Http/Controllers/Web/ReportExportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Services\ReportGeneratorService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ReportExportController extends Controller
@@ -12,6 +13,17 @@ class ReportExportController extends Controller
     public function __construct(
         protected ReportGeneratorService $reportService
     ) {}
+
+    /**
+     * Download Compliance Audit Report PDF.
+     */
+    public function exportPdf(Request $request): Response
+    {
+        $user = $request->user();
+        $unitId = $request->filled('unit_id') ? (int) $request->input('unit_id') : null;
+
+        return $this->reportService->exportComplianceSummaryPdf($user, $unitId);
+    }
 
     /**
      * Download Compliance Report CSV.
